3e063698341bcbe173f7f16f650a36fac736f0d9
BarathRaj26 <151170282+BarathRaj26@users.noreply.github.com>
2023-12-15 19:59:48 +0530

    I declared button outside onCreate function

--- app/src/main/java/in/edu/ptuniv/MainActivity.java
+++ app/src/main/java/in/edu/ptuniv/MainActivity.java
@@ -9,15 +9,17 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    Button button,button2,button3,button4,button6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button6 = findViewById(R.id.button6);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button6 = findViewById(R.id.button6);
         Button button7 = findViewById(R.id.button7);
     }
     public void syll_button(View view)
