8f1c51ad33ca7cf3282821f5557d48cdc7382389
kishen2003 <kishen2003@gmail.com>
2023-12-21 20:05:14 +0530

    Implemented Activity Life Cycle for MainActivity.java Overridden methods display a log info message in logcat

--- app/src/main/java/in/edu/ptuniv/MainActivity.java
+++ app/src/main/java/in/edu/ptuniv/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,7 +22,45 @@ public class MainActivity extends AppCompatActivity {
         button4 = findViewById(R.id.button4);
         button5 = findViewById(R.id.button5);
         button6 = findViewById(R.id.button6);
+        Log.i(TAG,"onCreate()");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart()");
+    }
+
     public void syll_button(View view)
     {
         Intent intent = new Intent(MainActivity.this, syllabus.class);
