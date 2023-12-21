10327d51b68e49a0ed929026aa33ffc86f66e1a5
kishen2003 <kishen2003@gmail.com>
2023-12-21 21:26:18 +0530

    Implemented fragment and fragment life cycle when you click about button, it goes to about activity and a fragment will display text

--- app/src/main/java/in/edu/ptuniv/about.java
+++ app/src/main/java/in/edu/ptuniv/about.java
@@ -2,13 +2,26 @@ package in.edu.ptuniv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class about extends AppCompatActivity {
+    private static final String TAG = about.class.getSimpleName();
+
+    private FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        manager = getFragmentManager();
+        fptupyqs frptupyqs = new fptupyqs();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.aboutActivity,frptupyqs,"fpyps");
+        transaction.commit();
     }
+
+
 }
