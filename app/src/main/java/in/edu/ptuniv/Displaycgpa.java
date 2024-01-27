d5310d48d669364b673c7be06b7e451a068ef43b
kishen2003 <kishen2003@gmail.com>
2024-01-28 00:15:54 +0530

    Changed context of cgpa calculator xml to cgpa calculator activity from main activity

---
+++ app/src/main/java/in/edu/ptuniv/Displaycgpa.java
@@ -0,0 +1,30 @@
+package in.edu.ptuniv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class Displaycgpa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaycgpa);
+        double gpaResult = getIntent().getDoubleExtra("GPA_RESULT", 0.0);
+        gpaResult = (double) Math.round(gpaResult * 100) /100;
+        String gpa = Double.toString(gpaResult);
+        TextView gpaResultTextView = findViewById(R.id.gpaResultTextView);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                gpaResultTextView.setText(String.format("Your GPA is: %s", gpa));
+                ObjectAnimator animator = ObjectAnimator.ofFloat(gpaResultTextView, "alpha", 0f, 1f);
+                animator.setDuration(3000);
+                animator.start();
+            }
+        }, 500);
+    }
+}
--- app/src/main/java/in/edu/ptuniv/cgpacalculator.java
+++ app/src/main/java/in/edu/ptuniv/cgpacalculator.java
@@ -2,8 +2,7 @@ package in.edu.ptuniv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +14,7 @@ public class cgpacalculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cgpacalculator);
         btn1 = findViewById(R.id.btn_sem1);
-        btn2 = findViewById(R.id.stn_sem2);
+        btn2 = findViewById(R.id.btn_sem2);
         btn3 = findViewById(R.id.btn_sem3);
         btn4 = findViewById(R.id.btn_sem4);
         btn5 = findViewById(R.id.btn_sem5);
@@ -25,22 +24,10 @@ public class cgpacalculator extends AppCompatActivity {
         btn9 = findViewById(R.id.cgpacalculator9);
     }
 
-    public void fsem1(View view)
+    public void csem1(View view)
     {
-        btn1.setVisibility(View.GONE);
-        btn2.setVisibility(View.GONE);
-        btn3.setVisibility(View.GONE);
-        btn4.setVisibility(View.GONE);
-        btn5.setVisibility(View.GONE);
-        btn6.setVisibility(View.GONE);
-        btn7.setVisibility(View.GONE);
-        btn8.setVisibility(View.GONE);
-        btn9.setVisibility(View.GONE);
-        FragmentManager manager = getFragmentManager();
-        fsem1 fsem1o = new fsem1();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.cgpacalc,fsem1o,"fsem1");
-        transaction.commit();
+        Intent intent = new Intent(cgpacalculator.this, csem1.class);
+        startActivity(intent);
     }
 
 }
